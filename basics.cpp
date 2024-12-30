a5cd71a1845849453ed2c556c83532107c7cf3a3
sahil0077 <sahilnwal975@gmail.com>
2024-12-30 13:30:59 +0530

    cout & next-line & c++ code run

---
+++ basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hllo c++ i am sahil" << "\n" << "now use other way to\nend line" << endl;
+    return 0;
+}
+
+// comments --> ctrl + /
+//compile cmd -- > g++ -o code filename.cpp
+// run cmd --> ./filename {which file is generated}
+
+// here we can compile and sun together by -- >  g++ -o code filename.cpp && ./filename 
