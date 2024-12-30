8dff3e63e5deff9f3c6c4b99c4ae49f75367f1c0
sahil0077 <sahilnwal975@gmail.com>
2024-12-30 14:21:43 +0530

    input in c++

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int age;
+    cout << age << endl;
+    cout << "what is your age? : ";
+    cin >> age;
+    cout << "your age is : " << age << endl;
+
+
+
+    return 0;
+}
